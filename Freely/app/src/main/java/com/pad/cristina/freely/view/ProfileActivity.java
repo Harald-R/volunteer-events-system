17564ca48c54f2739978788eba05cb06a287b751
Cristina Chindriș <cristinachindris@rocketmail.com>
2019-05-20 01:04:34 +0300

    mobile-app: Add menu + improvements

--- Freely/app/src/main/java/com/pad/cristina/freely/view/ProfileActivity.java
+++ Freely/app/src/main/java/com/pad/cristina/freely/view/ProfileActivity.java
@@ -1,7 +1,14 @@
 package com.pad.cristina.freely.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.pad.cristina.freely.R;
 
@@ -11,5 +18,26 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_profile:
+                        Intent intent1 = new Intent(ProfileActivity.this, ProfileActivity.class);
+                        startActivity(intent1);
+                        break;
+                    case R.id.action_events:
+                        Intent intent3 = new Intent(ProfileActivity.this, DashboardActivity.class);
+                        startActivity(intent3);
+                        break;
+                    case R.id.action_favorites:
+                        Intent intent2 = new Intent(ProfileActivity.this, DashboardActivity.class);
+                        startActivity(intent2);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
