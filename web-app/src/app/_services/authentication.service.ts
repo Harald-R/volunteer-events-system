5cbd5fb101498a3c1284f9cd05765f53a19bb4ed
Harald-R <razvan.harald@yahoo.com>
2019-05-20 19:24:48 +0300

    web-branch: Fix POST request to server

--- web-app/src/app/_services/authentication.service.ts
+++ web-app/src/app/_services/authentication.service.ts
@@ -2,6 +2,7 @@
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { timeout } from 'rxjs/operators';
 
 import { environment } from '@environments/environment';
 import { User } from '@app/_models';
@@ -25,7 +26,14 @@ export class AuthenticationService {
          console.log(JSON.stringify({email, password}));
          const c = 'email=' + email + '&password=' + password;
          console.log(c);
-         return this.http.post<any>('http://localhost:3000/api/auth/login', c , {headers: new  HttpHeaders('Content-Type: application/x-www-form-urlencoded')});
+
+        return this.http.post<any>('http://localhost:3000/api/auth/login', c, {
+            headers: new  HttpHeaders ({
+                'Content-Type': 'application/x-www-form-urlencoded'
+            })
+        }).pipe(
+            timeout(5000) //5 seconds
+        );
     }
     
     logout() {
