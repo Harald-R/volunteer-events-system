0b2aa416031ec994fca72b6c0ae7662e45e9e386
Berzescu Mirela <berzescumirela22@yahoo.com>
2019-05-20 21:54:38 +0300

    web-branch: Add events services

--- web-app/src/app/events.service.ts
+++ web-app/src/app/events.service.ts
@@ -8,17 +8,42 @@ import {IEvent} from '@app/_models/event';
 
 export class EventsService {
   private url = 'http://localhost/3000/api/events';
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient) {
+  }
+
 
   getEvents(): Observable<Array<IEvent>> {
-       return this.http.get<Array<IEvent>>(this.url, {headers: new HttpHeaders('Content-Type: application/x-www-form-urlencoded')});
+    return this.http.get<Array<IEvent>>(this.url, {headers: new HttpHeaders('Content-Type: application/x-www-form-urlencoded')});
   }
 
   addEvent(event: IEvent): Observable<IEvent> {
     const name = event.name;
-    const descr = event.description;
-    const loc = event.location;
+    const description = event.description;
+    const location = event.location;
     const date = event.date;
-    return  this.http.post<IEvent>(this.url, { name , descr, loc, date}, { headers : new HttpHeaders('Content-Type: application/x-www-form-urlencoded, x-access-token: $event.token')});
+    const c = 'name=' + name + '$date=' + date + '$description=' + description + '$location=' + location;
+    console.log(c);
+
+    return this.http.post<IEvent>(this.url, c, {headers: new HttpHeaders(`Content-Type: application/x-www-form-urlencoded, x-access-token: ${event.token}`)});
   }
+
+  getEvent(id: number): Observable<IEvent> {
+
+    const url = this.url + '/' + id;
+    return this.http.get<IEvent>(url, {headers: new HttpHeaders(`Content-Type: application/x-www-form-urlencoded`)});
+  }
+
+  updateEvent(event: IEvent): Observable<IEvent> {
+    const name = event.name;
+    const description = event.description;
+    const location = event.location;
+    const date = event.date;
+
+    const url = this.url + '/' + event._id;
+    const c = 'name=' + name + '&description=' + description + '&location=' + location + '&date' + date;
+
+    return this.http.put<IEvent>(url, c, {headers: new HttpHeaders(`Content-Type: application/x-www-form-urlencoded, x-access-token: ${event.token}`)});
+  }
+
 }
