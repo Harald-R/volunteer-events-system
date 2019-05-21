bd91128f296e63f069e237137cd085881f321473
Harald-R <razvan.harald@yahoo.com>
2019-05-21 10:56:03 +0300

    backend-server: Add CORS package

--- backend-server/server.js
+++ backend-server/server.js
@@ -4,14 +4,22 @@ var express = require('express'),
     mongoose = require('mongoose'),
     User = require('./api/models/users.model'),
     Event = require('./api/models/events.model'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    cors = require('cors');
 
 // mongos instance url connection
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/backend-server-db');
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 var route_auth = require('./api/routes/auth.route');
 var route_users = require('./api/routes/users.route');
