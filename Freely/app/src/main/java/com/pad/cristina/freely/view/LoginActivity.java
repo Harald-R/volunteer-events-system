c5b65057bf92936ea159565fe0aa8da2fb828aa9
Cristina Chindriș <cristinachindris@rocketmail.com>
2019-05-21 11:56:14 +0300

    mobile-app: Add login functionality

--- Freely/app/src/main/java/com/pad/cristina/freely/view/LoginActivity.java
+++ Freely/app/src/main/java/com/pad/cristina/freely/view/LoginActivity.java
@@ -1,9 +1,6 @@
 package com.pad.cristina.freely.view;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.SyncStatusObserver;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
@@ -11,13 +8,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.pad.cristina.freely.R;
+import com.pad.cristina.freely.model.UserInfo;
+import com.pad.cristina.freely.util.ValidityUtils;
 
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -36,31 +32,21 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         final Button btnLogin = findViewById(R.id.buttonLogin);
         final EditText emailEditTxt = findViewById(R.id.email);
-
+        final EditText passwEditTxt = findViewById(R.id.password);
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        String responseJsonStr=getJSON("http://192.168.43.192:3000/api/auth/login");
-        String token="";
-        try {
-            JSONParser parser = new JSONParser();
-            JSONObject jsonObject = (JSONObject)parser.parse(responseJsonStr);
-            token= (String) jsonObject.get("token");
-        }catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-        check_login_validity(btnLogin, emailEditTxt);
+        login(btnLogin, emailEditTxt, passwEditTxt);
     }
-    public static String getJSON(String url) {
+    public static String getJSON(String url, String email, String password) {
         HttpURLConnection con = null;
         try {
             URL u = new URL(url);
             con = (HttpURLConnection) u.openConnection();
             con.setRequestMethod("POST");
             con.setDoOutput(true);
-            con.getOutputStream().write("email=berzescumirela@yahoo.com&password=mirela".getBytes());
+            con.getOutputStream().write(("email="+email+"&password="+password).getBytes());
             con.connect();
 
             if (con.getResponseCode() == 200) {
@@ -88,16 +74,31 @@ public class LoginActivity extends AppCompatActivity {
         }
         return null;
     }
-    private void check_login_validity(Button btnRegister, final EditText emailEditTxt) {
+    private void login(Button btnRegister, final EditText emailEditTxt, final EditText passwEditTxt) {
         btnRegister.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                String txt = emailEditTxt.getText().toString();
-               /* if(!ValidityUtils.isEmailValid(txt))
-                    Toast.makeText(LoginActivity.this,"Email-ul "+txt+" este gresit",Toast.LENGTH_SHORT).show();
-                else{*/
-                    Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
-                    startActivity(intent);
-                //}
+                String email = emailEditTxt.getText().toString();
+                if (!ValidityUtils.isEmailValid(email)) {
+                    Toast.makeText(LoginActivity.this, "Email-ul " + email + " este gresit", Toast.LENGTH_SHORT).show();
+                } else {
+                    String password = passwEditTxt.getText().toString();
+                    String responseJsonStr = getJSON("http://192.168.43.192:3000/api/auth/login", email, password);
+                    String token = null;
+                    try {
+                        JSONParser parser = new JSONParser();
+                        JSONObject jsonObject = (JSONObject) parser.parse(responseJsonStr);
+                        token = (String) jsonObject.get("token");
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                    if (token != null) {
+                        UserInfo.setToken(token);
+                        Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
     }
